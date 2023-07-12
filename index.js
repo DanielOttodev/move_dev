48019a780e43e3696232fe6a6f9212ef54d3e5ea
Daniel Otto <44077055+DanielOttodev@users.noreply.github.com>
2023-07-13 09:29:15 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 80;
 const path = require("path");
 const bodyParser = require("body-parser")
 const csrf = require('csurf');
